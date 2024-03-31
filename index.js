2059d7a502bf81a94656227b5087eb5fd9cc269a
Akisa-Moruri <akisamoruri@gmail.com>
2024-03-31 07:42:13 +0300

    initialized modifying index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const element = document.querySelector('#main');
+element.remove ()
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = '<h1>Akisa</h1>is the champion';
+
+
